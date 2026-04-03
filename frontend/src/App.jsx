20d4ffa39a404f7adb40ab604ba6bac0c7918eee
Alfa-Anshul <anshul.pandey@Anervea.ai>
2026-04-03 17:50:59 +0530

    feat: dynamic animated scientific calculator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Calculator from './components/Calculator';
-
 export default function App() {
-  return <Calculator />;
+  return <div style={{position:'relative',zIndex:1}}><Calculator /></div>;
 }
